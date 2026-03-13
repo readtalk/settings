f0df29eb06843506c9f64b434d18e12507cefe37
READTalk <redtalk.app@hotmail.com>
2026-03-13 09:04:10 +0700

    Update main.tsx

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>    
+      <App />    
+  </StrictMode>,
+)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(reg => console.log('SW registered: ', reg))
+      .catch(err => console.log('SW registration failed: ', err));
+  });
+}
